a9c06853b89c5ff9b1ce0290d1413aa06b03be8f
Joshua Navi <jnavi@ucsd.edu>
2016-01-29 11:25:43 -0800

    Moved js

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -11,3 +11,9 @@ $(document).ready(function() {
 function initializePage() {
 	// add any functionality and listeners you want here
 }
+
+$(document).ready(function(){
+  console.log("suh dude");
+  var imgindex = Math.floor((Math.random() * 2) + 2);
+  $('html').css('background', "../images/splash" + imgindex + ".jpg");
+})
