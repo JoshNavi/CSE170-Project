2a8658c2bddafd027b8e2dbda15350e5fc68e710
Joshua Navi <jnavi@ucsd.edu>
2016-02-01 16:07:40 -0800

    Randomized pictures

--- public/js/splash.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  console.log("suh dude");
-  var imgindex = Math.floor((Math.random() * 2) + 2);
-  $('html').css('background', "../images/splash" + imgindex + ".jpg");
-})
