b079740fb5d16722d6935f7627f1146ccec3577a
Joshua Navi <jnavi@ucsd.edu>
2016-02-04 15:23:31 -0800

    Adding login page

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var handlebars = require('express3-handlebars')
 
 // routes
 var index = require('./routes/index');
+var login = require('./routes/login');
 // Example route
 // var user = require('./routes/user');
 
@@ -37,6 +38,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/login', login.view);
 // Example route
 // app.get('/users', user.list);
 
