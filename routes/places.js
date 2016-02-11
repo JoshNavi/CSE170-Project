2ef764c2f7944c427684aaf5cec957f292e9a768
Joshua Navi <jnavi@ucsd.edu>
2016-02-10 17:28:35 -0800

    Getting data for list view from server

--- routes/places.js
+++ routes/places.js
@@ -1,43 +1,12 @@
 
 var places = require('../places.json');
 var pl = {
-  'places':
-  [
-    {
-      "id": 1,
-      "name": "Place 1",
-      "description": "This is a dope place",
-      "image": "http://www.planwallpaper.com/static/images/4-Nature-Wallpapers-2014-1_cDEviqY.jpg",
-      "rating": 5
-    },
-    {
-      "id": 2,
-      "name": "Place 2",
-      "description": "This is a dope place",
-      "image": "http://www.planwallpaper.com/static/images/4-Nature-Wallpapers-2014-1_cDEviqY.jpg",
-      "rating": 5
-    },
-    {
-      "id": 3,
-      "name": "Place 3",
-      "description": "This is a dope place",
-      "image": "http://www.planwallpaper.com/static/images/4-Nature-Wallpapers-2014-1_cDEviqY.jpg",
-      "rating": 5
-    },
-    {
-      "id": 4,
-      "name": "Place 4",
-      "description": "This is a dope place",
-      "image": "http://www.planwallpaper.com/static/images/4-Nature-Wallpapers-2014-1_cDEviqY.jpg",
-      "rating": 5
-    }
-  ]
-
+  'places': places
 };
 
 exports.view = function(req, res){
   console.log(places);
   console.log(pl);
   console.log("Anything pls");
-  res.render('places', pl);
+  res.render('places', places);
 };
