83f903638664fbba1494e5a24fbfb4ad2ce7f076
Joshua Navi <jnavi@ucsd.edu>
2016-02-10 17:32:55 -0800

    Getting data for list view from server

--- routes/places.js
+++ routes/places.js
@@ -1,12 +1,9 @@
 
 var places = require('../places.json');
-var pl = {
+var data = {
   'places': places
 };
 
 exports.view = function(req, res){
-  console.log(places);
-  console.log(pl);
-  console.log("Anything pls");
-  res.render('places', pl);
+  res.render('places', data);
 };
