506b25f4d48b021ed861de2b31714798c72af206
Joshua Navi <jnavi@ucsd.edu>
2016-02-18 13:04:56 -0800

    Carousel

--- routes/homepage.js
+++ routes/homepage.js
@@ -4,29 +4,29 @@
  */
 
 exports.view = function(req, res){
-  var data = {
-    "pics": [
-      {
-        "name": "/images/bear.jpg"
-      },
-      {
-        "name": "/images/midgethouse.jpg"
-      },
-      {
-        "name": "/images/mushyhouse.jpg"
-      },
-      {
-        "name": "/images/redchair.jpg"
-      },
-      {
-        "name": "/images/salk.jpg"
-      },
-      {
-        "name": "/images/sioswng.jpg"
-      }
-    ]
-  }
-  res.render('homepage', data);
+  // var data = {
+  //   "pics": [
+  //     {
+  //       "name": "/images/bear.jpg"
+  //     },
+  //     {
+  //       "name": "/images/midgethouse.jpg"
+  //     },
+  //     {
+  //       "name": "/images/mushyhouse.jpg"
+  //     },
+  //     {
+  //       "name": "/images/redchair.jpg"
+  //     },
+  //     {
+  //       "name": "/images/salk.jpg"
+  //     },
+  //     {
+  //       "name": "/images/sioswng.jpg"
+  //     }
+  //   ]
+  // }
+  res.render('homepage');
 };
 
 
