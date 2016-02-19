2605e28143142c95ff068d4e32d85f2f5f3a8cdb
Joshua Navi <jnavi@ucsd.edu>
2016-02-18 17:10:37 -0800

    upload

--- routes/places.js
+++ routes/places.js
@@ -16,18 +16,19 @@ exports.add = function(req, res){
   //   'places': places
   // };
   var id = places.length;
-  // var newPlace = {
-  //   "id": id,
-  //   "name": req.body.name,
-  //   "description": req.body.description,
-  //   "image" : req.body.image,
-  //   "ratings" : {
-  //     "overall": req.body.overall,
-  //     "secluded": req.body.secluded,
-  //     "romantic": req.body.romantic,
-  //     "quiet": req.body.quiet
-  // }
-  // places.push(newPlace)
+  var newPlace = {
+    "id": id,
+    "name": req.body.name,
+    "description": req.body.description,
+    "image" : req.body.image,
+    "ratings" : {
+      "overall": req.body.overall,
+      "secluded": req.body.secluded,
+      "romantic": req.body.romantic,
+      "quiet": req.body.quiet
+    }
+  };
+  places.push(newPlace)
   var data = {
     "places": places
   };
