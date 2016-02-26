da754023228d43c74f86228762b598bc88e303e9
Joshua Navi <jnavi@ucsd.edu>
2016-02-25 23:05:46 -0800

    Style changes

--- public/js/map.js
+++ public/js/map.js
@@ -7,9 +7,52 @@ function initMap() {
     disableDefaultUI: true
   });
 
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
-  });
+  var places = [
+    {
+      "name" : "Boulder Bear",
+      "lat": 32.881971,
+      "lng": -117.234152,
+      "link": 0
+    },
+    {
+      "name" : "Mushroom House",
+      "lat": 32.887690,
+      "lng": -117.252916,
+      "link": 1
+    },
+    {
+      "name" : "Red Chair",
+      "lat": 32.873217,
+      "lng": -117.241254,
+      "link": 2
+    },
+    {
+      "name" : "Salk Institute",
+      "lat": 32.887411,
+      "lng": -117.245331,
+      "link": 3
+    },
+    {
+      "name" : "Munchkin House",
+      "lat": 32.844426,
+      "lng": -117.256618,
+      "link": 5
+    }
+  ];
+
+  for( var i = 0; i < places.length; i++) {
+    var pos = {
+      lat: places[i].lat,
+      lng: places[i].lng
+    };
+
+    var marker = new google.maps.Marker({
+      map: map,
+      position: pos,
+      title: places[i].name,
+      url: "http://projectgooutside.herokuapp.com/places/" + places[i].link
+    });
+
+  }
+
 }
